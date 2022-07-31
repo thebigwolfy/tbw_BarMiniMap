fb8951a67dfd6c3c0a38d1fa4efc5999eacf9c77
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-07-31 12:44:35 +0200

    v0.0.1
    Ajout du script de base

---
+++ client.lua
@@ -0,0 +1,30 @@
+-- Crédits --
+--
+-- Créateur : TheBigWolfy
+-- Script du pack TBW
+--
+-- Crédits --
+
+Citizen.CreateThread(function()
+
+	local minimap = RequestScaleformMovie("minimap")
+
+    SetRadarBigmapEnabled(true, false)
+
+    Wait(0)
+
+    SetRadarBigmapEnabled(false, false)
+
+    while true do
+
+        Wait(0)
+
+        BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+
+        ScaleformMovieMethodAddParamInt(3)
+
+        EndScaleformMovieMethod()
+
+    end
+
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Crédits --
+--
+-- Créateur : TheBigWolfy
+-- Script du pack TBW
+--
+-- Crédits --
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheBigWolfy'
+description 'Ressource du pack TBW - Retrait de la barre de vie classique.'
+version '0.0.1'
+
+client_scripts {
+    'client.lua'
+}
